d1ea975272af448839551c01a6015fa91657175e
Shuhaib N C <56123591+ShuhaibNC@users.noreply.github.com>
2023-02-26 16:19:46 +0530

    Add files via upload

---
+++ WebStreamer/bot/plugins/start.py
@@ -0,0 +1,237 @@
+
+from WebStreamer.bot import StreamBot
+from WebStreamer.vars import Var
+from WebStreamer.utils.human_readable import humanbytes
+from WebStreamer.utils.database import Database
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant
+db = Database(Var.DATABASE_URL, Var.SESSION_NAME)
+
+
+@StreamBot.on_message(filters.command('start') & filters.private & ~filters.edited)
+async def start(b, m):
+    if not await db.is_user_exist(m.from_user.id):
+        await db.add_user(m.from_user.id)
+        await b.send_message(
+            Var.BIN_CHANNEL,
+            f"👥 New User: \n\nNew User [{m.from_user.first_name}](tg://user?id={m.from_user.id}) Started !!"
+        )
+    usr_cmd = m.text.split("_")[-1]
+    if usr_cmd == "/start":
+        if Var.UPDATES_CHANNEL is not None:
+            try:
+                user = await b.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
+                if user.status == "banned":
+                    await b.send_message(
+                        chat_id=m.chat.id,
+                        text="You are Banned to use me. Contact @ShuhaibNC.",
+                        parse_mode="markdown",
+                        disable_web_page_preview=True
+                    )
+                    return
+            except UserNotParticipant:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!\n\n@ShuhaibChannel",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("🤖 Join Updates Channel", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                            ]
+                        ]
+                    ),
+                    parse_mode="markdown"
+                )
+                return
+            except Exception:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="Something went Wrong. Contact @ShuhaibNC.",
+                    parse_mode="markdown",
+                    disable_web_page_preview=True)
+                return
+        await m.reply_text(
+            text="🙋 Hey Bruh!!\n\nI am Instant Telegram File to Link Generator Bot.\n\nSend me any file & see the magic!",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton("Channel ", url="https://t.me/MovieCrackerLinks"), InlineKeyboardButton("Support", url="https://t.me/MovieCrackerLinks")],
+                    [InlineKeyboardButton("Developer", url="https://t.me/ShuhaibNC")]
+                    
+                ]
+            ),
+            disable_web_page_preview=True
+        )
+    else:
+        if Var.UPDATES_CHANNEL is not None:
+            try:
+                user = await b.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
+                if user.status == "banned":
+                    await b.send_message(
+                        chat_id=m.chat.id,
+                        text="You are Banned to use me. Contact @ShuhaibNC.",
+                        parse_mode="markdown",
+                        disable_web_page_preview=True
+                    )
+                    return
+            except UserNotParticipant:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!\n\n@ShuhaibChannel",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("🤖 Join Updates Channel", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                            ],
+                            [
+                                InlineKeyboardButton("🔄 Refresh 🔄",
+                                                     url=f"https://t.me/TeleRoid_File2Link_Bot?start=PredatorHackerzZ_{usr_cmd}")
+                            ]
+                        ]
+                    ),
+                    parse_mode="markdown"
+                )
+                return
+            except Exception:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="Something went Wrong. Contact @ShuhaibNC.",
+                    parse_mode="markdown",
+                    disable_web_page_preview=True)
+                return
+
+        get_msg = await b.get_messages(chat_id=Var.BIN_CHANNEL, message_ids=int(usr_cmd))
+
+        file_size = None
+        if get_msg.video:
+            file_size = f"{humanbytes(get_msg.video.file_size)}"
+        elif get_msg.document:
+            file_size = f"{humanbytes(get_msg.document.file_size)}"
+        elif get_msg.audio:
+            file_size = f"{humanbytes(get_msg.audio.file_size)}"
+
+        file_name = None
+        if get_msg.video:
+            file_name = f"{get_msg.video.file_name}"
+        elif get_msg.document:
+            file_name = f"{get_msg.document.file_name}"
+        elif get_msg.audio:
+            file_name = f"{get_msg.audio.file_name}"
+
+        stream_link = "https://{}/{}".format(Var.FQDN, get_msg.message_id) if Var.ON_HEROKU or Var.NO_PORT else \
+            "http://{}:{}/{}".format(Var.FQDN,
+                                     Var.PORT,
+                                     get_msg.message_id)
+
+        msg_text = "**File Link Generated!**\n\n📂 **File Name:** `{}`\n📦**File Size:** `{}`\n\n📥 **Download Link:** `{}`\n\n@ShuhaibChannel"
+        await m.reply_text(
+            text=msg_text.format(file_name, file_size, stream_link),
+            parse_mode="Markdown",
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Download", url=stream_link)]])
+        )
+
+
+@StreamBot.on_message(filters.command('help') & filters.private & ~filters.edited)
+async def help_handler(bot, message):
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id)
+        await bot.send_message(
+            Var.BIN_CHANNEL,
+            f"#NEW_USER: \n\nNew User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) Started !!"
+        )
+    if Var.UPDATES_CHANNEL is not None:
+        try:
+            user = await bot.get_chat_member(Var.UPDATES_CHANNEL, message.chat.id)
+            if user.status == "banned":
+                await bot.send_message(
+                    chat_id=message.chat.id,
+                    text="You are Banned to use me. Contact @ShuhaibNC.",
+                    parse_mode="markdown",
+                    disable_web_page_preview=True
+                )
+                return
+        except UserNotParticipant:
+            await bot.send_message(
+                chat_id=message.chat.id,
+                text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("🤖 Join Updates Channel", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                        ]
+                    ]
+                ),
+                parse_mode="markdown"
+            )
+            return
+        except Exception:
+            await bot.send_message(
+                chat_id=message.chat.id,
+                text="Something went Wrong. Contact @ShuhaibNC.",
+                parse_mode="markdown",
+                disable_web_page_preview=True)
+            return
+    await message.reply_text(
+        text="⚡Sᴇɴᴅ ᴍᴇ ᴀɴʏ ꜰɪʟᴇ (ᴏʀ) ᴍᴇᴅɪᴀ ꜰʀᴏᴍ ᴛᴇʟᴇɢʀᴀᴍ....\nThis Bot Will Send You Permanent Link\n⚡This Link Can Be Used To Download Or Stream Files[Using External Video Player] Through My Server\n⚡For Streaming Just Copy The Mono Link And Paste It In Your Video Player To Start Streaming\n⚡This bot shAres the Permanent Link to yOu.\n⚡This Bot Is Also Supported in Channels. Add me to Channel as Admin to Make Me Workable!\nFor More Information: @TeleRoidGroup\n\n🔹**WARNING**🚸\n**🔞 Porn Contents Leads You To Permanent Ban From All Bots**", 
+  parse_mode="Markdown",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("⛴️ Channel ⛴️", url="https://t.me/MovieCrackerLinks"), InlineKeyboardButton("Support", url="https://t.me/ShuhaibChannel")],
+                [InlineKeyboardButton("🦾 Powered By ", url="https://t.me/MovieCrackerLinks"), InlineKeyboardButton("💰 Donate", url="https://t.me/ShuhaibncBot")],
+                [InlineKeyboardButton("👨‍💻 Developer", url="https://t.me/ShuhaibNC")]
+            ]
+        )
+    )
+
+@StreamBot.on_message(filters.command('about') & filters.private & ~filters.edited)
+async def about_handler(bot, message):
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id)
+        await bot.send_message(
+            Var.BIN_CHANNEL,
+            f"#NEW_USER: \n\nNew User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) Started !!"
+        )
+    if Var.UPDATES_CHANNEL is not None:
+        try:
+            user = await bot.get_chat_member(Var.UPDATES_CHANNEL, message.chat.id)
+            if user.status == "banned":
+                await bot.send_message(
+                    chat_id=message.chat.id,
+                    text="You are Banned to use me. Contact @ShuhaibNC.",
+                    parse_mode="markdown",
+                    disable_web_page_preview=True
+                )
+                return
+        except UserNotParticipant:
+            await bot.send_message(
+                chat_id=message.chat.id,
+                text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("🤖 Join Updates Channel", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                        ]
+                    ]
+                ),
+                parse_mode="markdown"
+            )
+            return
+        except Exception:
+            await bot.send_message(
+                chat_id=message.chat.id,
+                text="Something went Wrong. Contact @ShuhaibNC.",
+                parse_mode="markdown",
+                disable_web_page_preview=True)
+            return
+    await message.reply_text(
+        text=""" ABOUT""",
+  parse_mode="html",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("🦾 Powered By ", url="https://t.me/MovieCrackerLinks"), InlineKeyboardButton("💰 Donate", url="https://t.me/ShuhaibNCbot")],
+                [InlineKeyboardButton("Need Private Bot ❓", url="https://t.me/ShuhaibNC")]
+            ]
+        )
+    )
